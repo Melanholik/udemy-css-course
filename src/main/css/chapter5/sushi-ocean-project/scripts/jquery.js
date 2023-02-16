8a0a9d80b63ea08f4c7d1bc7dcc98d74b73ba4fb
Melanholik <Forliho@gmail.com>
2023-02-16 11:32:32 +0300

    scroll JQuery stage 69

--- src/main/css/chapter5/sushi-ocean-project/scripts/jquery.js
+++ src/main/css/chapter5/sushi-ocean-project/scripts/jquery.js
@@ -1,15 +1,62 @@
 $(document).ready(function () {
+    //sticky-nav
     new Waypoint({
         element: document.getElementById('restaurant-description'),
         handler: function (direction) {
-            if (direction === "down") {
-                $('nav').attr('id', "sticky-nav");
+            if (direction === 'down') {
+                $('nav').attr('id', 'sticky-nav');
             } else {
                 $('nav').removeAttr('id');
             }
         },
         offset: 60
     });
+
+    $(".button-order").click(function () {
+        $("html, body").animate({scrollTop: $("#how-to-order").offset().top}, 800);
+    });
+
+    $(".button-menu").click(function () {
+        $("html, body").animate({scrollTop: $("#restaurant-description").offset().top}, 800);
+    });
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+                &&
+                location.hostname === this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        }
+                    });
+                }
+            }
+        });
+
 })
 
 
